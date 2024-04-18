ec66fafcc801d316570c024fec0344abb256b882
Therealcorwin <therealcorwin@gmail.com>
2024-04-18 15:18:34 +0200

    liste et id des livres a 1 *

--- scraping/Check_star.py
+++ scraping/Check_star.py
@@ -1,4 +1,5 @@
 import requests
+import re
 from bs4 import BeautifulSoup
 from pprint import pprint
 
@@ -8,5 +9,27 @@ Response = Session_scrapping.get(url)
 soup = BeautifulSoup(Response.text, "html.parser")
 
 one_star_book = soup.select("p.star-rating.One")
-for book in one_star_book:
-    print(book.find_next("h3").find("a")["href"])
+for livre in one_star_book:
+    try:
+        lien_livre = livre.find_next("h3").find("a")["href"]
+    except AttributeError as e:
+        print("Impossible de trouver la balise h3 ou a.")
+        raise AttributeError from e
+    except KeyError as e:
+        print("Impossible de trouver l'attribut href.")
+        raise KeyError from e
+    try:
+        titre_livre = livre.find_next("h3").find("a")["title"]
+    except AttributeError as e:
+        print("Impossible de trouver la balise h3 ou title.")
+        raise AttributeError from e
+    except KeyError as e:
+        print("Impossible de trouver l'attribut title.")
+        raise KeyError from e
+    try:
+        livre_id = re.findall(r"_\d+", lien_livre)[0][1:]
+        # r"_\d+" == r"_\d{1,}
+        print(f"titre à enlever : {titre_livre} ==> ID: {livre_id}")
+    except IndexError as e:
+        print("Impossible de trouver l'ID du livre.")
+        raise IndexError from e
